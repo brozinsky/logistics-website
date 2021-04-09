52ccbfd58e531df50a3f663dad4888606533f77a
Mateusz Brzeziski <mateusz0brzezinski@gmail.com>
2021-04-09 15:18:02 +0200

    add scrolling animations

--- main.js
+++ main.js
@@ -1,3 +1,26 @@
+//Animations
+
+const headerArrow = document.querySelector('.scroll')
+const sections = document.querySelectorAll('section')
+
+//header scroll arrow animation
+gsap.from(headerArrow, 2, { delay: 1.4, opacity: 0, })
+gsap.from(headerArrow, 1.5, { delay: 0, y: '-20px', repeat: -1, yoyo: true, ease: "slow(0.7, 0.7, false)", });
+
+
+//all sections animation
+sections.forEach(section => {
+  gsap.fromTo(section.children, { y: '+=100', opacity: 0 },
+    {
+      y: 0, opacity: 1, stagger: 0.2, duration: 0.8,
+      scrollTrigger: {
+        trigger: section,
+        start: 'top 65%',
+        ease: 'power3. out'
+      }
+    })
+})
+
 const menu = document.querySelector('.menu');
 const burger = document.querySelector('.burger-menu');
 const links = menu.querySelectorAll("a");
@@ -29,3 +52,4 @@ links.forEach(link => {
     menu.classList.toggle("menu--open");
   });
 });
+
